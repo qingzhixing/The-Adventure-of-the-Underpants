78ee1d0cb364bb50b5a9dd4b9d9337150486405d
qingzhixing <qzsqqmail@qq.com>
2024-05-02 11:50:12 +0800

    🩲 写入test.json成功！

--- include/global_data_manager.hpp
+++ include/global_data_manager.hpp
@@ -9,6 +9,7 @@ class GlobalDataManager
 public:
     void load_player_data();
     void save_player_data();
+    static const char *get_exe_dir();
 
 public:
     Player player_data;
--- source/global_data_manager.cpp
+++ source/global_data_manager.cpp
@@ -2,7 +2,10 @@
 
 #include <iostream>
 #include <fstream>
-#include "rapidjson/document.h"
+#include <direct.h>
+#include <rapidjson/document.h>
+#include <rapidjson/filewritestream.h>
+#include <rapidjson/prettywriter.h>
 
 using namespace rapidjson;
 
@@ -13,19 +16,28 @@ void GlobalDataManager::load_player_data()
     printf("Unimplemented: load_player_data\n");
 }
 
+const char *GlobalDataManager::get_exe_dir()
+{
+    static char path[1024];
+    _getcwd(path, sizeof(path));
+    return path;
+}
+
 void GlobalDataManager::save_player_data()
 {
     printf("Unimplemented: save_player_data\n");
     Document doc;
-    doc.SetObject();
 
-    // FIXME!
     Value value(kObjectType);
     value.AddMember("Author", "qingzhixing sama!", doc.GetAllocator());
 
-    doc.AddMember("Object", value, doc.GetAllocator());
+        FILE *file = fopen("test.json", "w");
 
-    FILE *file = fopen("test.json", "w");
-    fprintf(file, "%s", doc.GetString());
+    char writeBuffer[65536];
+    FileWriteStream os(file, writeBuffer, sizeof(writeBuffer));
+
+    PrettyWriter<FileWriteStream> writer(os); // 注意，可以不使用PrettyWriter，不过，写出来的结构不好看，Pretty会将json写成树结构
+    doc.Accept(writer);
+
     fclose(file);
 }
